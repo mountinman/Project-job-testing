2ce44cfcb923e36d97de805f4fcdebcc8324a617
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-23 12:12:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Locastic Job Interview Project
+
+A simple responsive site
+
+### Version
+1.0.0
+
+## Usage
+
+
+### Installation
+
+Install the dependencies (gulp, gulp-sass, browser-sync)
+
+```sh
+$ npm install
+```
+
+### Run
+
+This will watch your sass files, compile them and run your dev server at http://localhost:3000
+
+```sh
+$ npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require("gulp-sourcemaps");
+var plumber = require("gulp-plumber");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var postcss = require('gulp-postcss');
+
+// Copy html file to dist folder
+gulp.task("copyHtml", function() {
+    gulp.src("src/*.html").pipe(gulp.dest("dist"));
+  });
+  
+  // Scripts
+  gulp.task("script", function() {
+    gulp
+      .src("src/js/*.js")
+      .pipe(sourcemaps.init())
+      .pipe(plumber())
+      .pipe(concat("main.js"))
+      .pipe(gulp.dest("dist/js")) 
+      .pipe(uglify())
+      .pipe(rename({ extname: ".min.js" }))
+      .pipe(sourcemaps.write("maps"))
+      .pipe(gulp.dest("dist/js"));
+  });
+
+//compile Sass and autoprefix compiled files for distribution (only clean code)
+gulp.task('dist', function(){
+    gulp.src('src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+          browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('dist/css'));
+  });  
+
+//compile Sass and autoprefix compiled files for development
+gulp.task('src', function(){
+    gulp.src('src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+          browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('src/css'));
+  });  
+
+//postcss-normalize
+gulp.task('css', function () {
+    gulp.src('src/css/*.css').pipe(
+        postcss([
+            require('postcss-normalize')({ browsers: 'last 3 versions'})
+        ])
+    ).pipe(
+        gulp.dest('dist/css')
+    );
+});    
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp.src(['src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./src"  
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Default Task
+gulp.task('default', ['serve']);
+gulp.task("sendDist", ["copyHtml", "script","dist", "css"]);
