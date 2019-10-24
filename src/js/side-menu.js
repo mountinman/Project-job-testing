606c73a8272918e4fca0eda0ebe1ad34684e0060
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-24 14:38:47 +0200

    carousel added

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -1,6 +1,7 @@
 $(function() {
     $('.header__hamburger-menu').click(function(){
         $('.header__main-nav').toggleClass('open');
+        $('.slider').toggleClass('gray');
         $("body").toggleClass("sidebar-open");
     });
    
---
+++ src/js/slider.js
@@ -0,0 +1,21 @@
+var slideIndex = 0;
+
+slideShow();
+function slideShow() {
+  var i;
+  var slides = document.getElementsByClassName("slider__slide");
+  var dots = document.getElementsByClassName("slider__dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(slideShow, 4000);
+}
