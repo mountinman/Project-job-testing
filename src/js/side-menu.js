41c12fa6c0f0db6a7576642bc040fec64a3273c0
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-24 16:29:22 +0200

    finishing desktop, tablet and mobile views for articles

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -1,7 +1,7 @@
 $(function() {
     $('.header__hamburger-menu').click(function(){
         $('.header__main-nav').toggleClass('open');
-        $('.slider').toggleClass('gray');
+        $('.slider, .article-container').toggleClass('on-sidebar-open');
         $("body").toggleClass("sidebar-open");
     });
    
